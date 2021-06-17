31530feb1895746d08f2edd03bdacb5ee529f369
Mark Huang <hhzeos@gmail.com>
2021-06-17 21:50:27 +0800

    Update

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_namespace_packages, setup
 
 MAJOR = 0
 MINOR = 0
-MICRO = 2
+MICRO = 3
 VERSION = "%d.%d.%d" % (MAJOR, MINOR, MICRO)
 
 with open("README.md", "r") as f:
@@ -13,7 +13,7 @@ setup(
     version=VERSION,
     author="Mark He Huang",
     author_email="mark.h.huang@gmail.com",
-    description="Python Utility Tool-kits",
+    description="Python Utility Tools",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/MarkHershey/puts",
