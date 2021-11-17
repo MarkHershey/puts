8fa7525efb7ff1320a5a319d6c2d993a00adb6d4
Mark Huang <hhzeos@gmail.com>
2021-11-17 14:12:37 +0800

    Update v0.0.7; Add deprecation warning

--- puts/__init__.py
+++ puts/__init__.py
@@ -1,17 +1,26 @@
-from ._logging import get_logger, init_logger
 from ._color import getHexColorList
 from ._encoding import (
-    is_ascii,
-    check_non_ascii_index,
-    check_non_ascii_char,
-    is_ascii_only_file,
     check_file_by_line,
+    check_non_ascii_char,
+    check_non_ascii_index,
     ensure_no_zh_punctuation,
+    is_ascii,
+    is_ascii_only_file,
     replace_punc_for_file,
 )
-from ._file import alternative_file_path, MassCopier
+from ._file import MassCopier, alternative_file_path
+from ._logging import get_logger, init_logger
 from ._string import title_cap, title_capitalize
-from ._time import timeit, timeitprint, timestamp_seconds, timestamp_microseconds
-from ._utils import convert_date_to_datetime, convert_datetime_to_date, json_serial
-from ._utils import print_green, print_yellow, print_red, print_cyan, print_bold
-from ._utils import printc, print_with_color
+from ._time import timeit, timeitprint, timestamp_microseconds, timestamp_seconds
+from ._utils import (
+    convert_date_to_datetime,
+    convert_datetime_to_date,
+    json_serial,
+    print_bold,
+    print_cyan,
+    print_green,
+    print_red,
+    print_with_color,
+    print_yellow,
+    printc,
+)
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_namespace_packages, setup
 
 MAJOR = 0
 MINOR = 0
-MICRO = 6
+MICRO = 7
 VERSION = "%d.%d.%d" % (MAJOR, MINOR, MICRO)
 
 with open("README.md", "r") as f:
