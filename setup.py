666f251520169da3c41c2ba3461901f479a718c3
Mark Huang <hhzeos@gmail.com>
2022-08-30 20:33:17 +0800

    Update v0.0.10

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_namespace_packages, setup
 
 MAJOR = 0
 MINOR = 0
-MICRO = 9
+MICRO = 10
 VERSION = "%d.%d.%d" % (MAJOR, MINOR, MICRO)
 
 with open("README.md", "r") as f:
@@ -11,7 +11,7 @@ with open("README.md", "r") as f:
 setup(
     name="puts",
     version=VERSION,
-    author="Mark He Huang",
+    author="Mark H. Huang",
     author_email="dev@markhh.com",
     description="Python Utility Tools",
     long_description=long_description,
